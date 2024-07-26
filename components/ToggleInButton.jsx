ca02b30f4c4c206eb3ed491f91633e423bd59abe
miha11-hash <d_mihaela@msn.com>
2024-07-26 06:21:20 +0300

    signOut

--- components/ToggleInButton.jsx
+++ components/ToggleInButton.jsx
@@ -7,6 +7,7 @@ import {
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { FaUserCircle } from "react-icons/fa";
+import { signOut } from "@/auth";
 
 function ToggleInButton() {
 
@@ -14,19 +15,19 @@ function ToggleInButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="w-15 text-lg">
         <Button variant="icon">
-          <FaUserCircle style={{fontSize: "1.5rem"}}></FaUserCircle>
+          <FaUserCircle style={{ fontSize: "1.5rem" }}></FaUserCircle>
           <span className="sr-only">Toggle Links</span>
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className="w-52" align="start" sideOffset={25}>
         <DropdownMenuItem>
-          <Link
-            href="/"
-            className="flex align-center gap-x-2 text-lg"
-          >
-            SignOut
-          </Link>
+          <form className="w-full" action={async () => {
+                "use server";
+                await signOut();
+              }}>
+            <Button type="submit" className="flex align-center gap-x-2 text-lg w-full">SignOut</Button>
+          </form>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
